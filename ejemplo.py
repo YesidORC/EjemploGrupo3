beb6f1e601ce8ac1abfe899f7da9f0807b92260f
YesidORC <yesid.ramirez@udea.edu.co>
2025-09-04 14:58:57 -0500

    Agregue ms lineas de ejemplo

--- ejemplo.py
+++ ejemplo.py
@@ -1,7 +1,12 @@
 print("hOLA mUNDO")
+print("hOLA m
 print("hOLA mUNDO")
 print("hOLA mUNDO")
 print("hOLA mUNDO")
 print("hOLA mUNDO")
 print("hOLA mUNDO")
 print("hOLA mUNDO")
+print("hOLA mUNDO")
+print("hOLA mUNDO")
+print("hOLA mUNDO")
+print("hOLA 
